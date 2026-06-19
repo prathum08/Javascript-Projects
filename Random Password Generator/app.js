cf1dc8c474b92ba3bdbc61a2b2ca4b0dd4b6056b
prathum08 <prathumbhangadia08@gmail.com>
2026-06-19 13:40:44 +0530

    Random Password Generator

---
+++ Random Password Generator/app.js
@@ -0,0 +1,36 @@
+const passwordBox = document.getElementById("password")
+
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+const number = "0123456789";
+
+const symbols = "!@#$%&*{}|\/?"
+
+const allChars = upperCase + lowerCase + number + symbols;
+
+function createPassword(){
+    let password = ""
+
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)]
+    }
+
+    passwordBox.value = password;
+
+    // console.log(password)
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy")
+
+}
